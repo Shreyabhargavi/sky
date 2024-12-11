c30d32f9f350683b3765ab1ebf7c334b3abf0406
Shreyabhargavi <shreyabhargavi35@gmail.com>
2024-12-11 16:11:11 +0530

    Update addition.java

--- addition.java
+++ addition.java
@@ -1,9 +1,29 @@
-import java.util.Scanner;
-public class addition {
-public static void main(String[] args)
-Scanner scanner = new Scanner(System.in);
-System.out.print "Enter the first number: "); double numi scanner.nextDouble();
-System.out.print("Enter the second number: "); double num2 scanner.nextDouble();
-double sum num1 + num2;
-System.out.println "The sum of numl + and num2 + is: + sum);
-scanner.close();
+public class addition{
+
+public static void main(String[] args) {
+
+if (args.length < 2) {
+
+System.out.println("Please provide two numbers as arguments.");
+
+return;
+
+}
+
+// Parse the numbers from command-line arguments
+
+int num1 = Integer.parseInt(args[0]);
+
+
+
+int num2= Integer.parseInt(args[1]);
+
+// Calculate the sum
+
+int sum num1 + num2;
+
+// Display the result
+
+System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+}
+}
