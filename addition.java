5cc5e0da1b31c48d052a8589efe8ae53747a6fcd
Shreyabhargavi <shreyabhargavi35@gmail.com>
2024-12-11 15:35:18 +0530

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class addition {
+public static void main(String[] args)
+Scanner scanner = new Scanner(System.in);
+System.out.print "Enter the first number: "); double numi scanner.nextDouble();
+System.out.print("Enter the second number: "); double num2 scanner.nextDouble();
+double sum num1 + num2;
+System.out.println "The sum of numl + and num2 + is: + sum);
+scanner.close();
